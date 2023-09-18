a6f95e959693cf65203ec78354eaa1b691f111c7
xWyvernPx <phonglethanh2@gmail.com>
2023-09-18 20:13:29 +0700

    [Setup] Add dto

--- LeThanhPhongWPF/obj/Debug/net7.0-windows/App.g.i.cs
+++ LeThanhPhongWPF/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace LeThanhPhongWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace LeThanhPhongWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             LeThanhPhongWPF.App app = new LeThanhPhongWPF.App();
             app.InitializeComponent();
